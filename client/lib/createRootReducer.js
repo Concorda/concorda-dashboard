e8c479ca0a51f0606cfb07782214230878b33378
Cristian Kiss <cristian.kiss@nearform.com>
2016-02-10 13:12:41 +0200

    removed sidebar

--- client/lib/createRootReducer.js
+++ client/lib/createRootReducer.js
@@ -4,7 +4,6 @@ import {combineReducers} from 'redux'
 
 import {routeReducer} from 'redux-simple-router'
 import authReducer from '../reducers/auth'
-import sidebarReducer from '../reducers/sidebar'
 import usersReducer from '../reducers/users'
 import profileReducer from '../reducers/profile'
 import {reducer as formReducer} from 'redux-form'
@@ -14,7 +13,6 @@ export default function createRootReducer () {
     routing: routeReducer,
     form: formReducer,
     auth: authReducer,
-    sidebar: sidebarReducer,
     users: usersReducer,
     profile: profileReducer
   })
