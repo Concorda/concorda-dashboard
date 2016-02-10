293bd2cc39e4f606200d51c23d384277f2d9a6b3
Cristian Kiss <cristian.kiss@nearform.com>
2016-02-10 10:12:48 +0200

    moved menu from header to separate component

--- client/components/header.js
+++ client/components/header.js
@@ -2,40 +2,11 @@
 
 import React from 'react'
 import {Link} from 'react-router'
+import Menu from './menu'
 
 export default React.createClass({
   render () {
-    const handleEditUserProfile = this.props.handleEditUserProfile
-    let profile = (
-      <div className="col-xs-4 col-sm-6 nav-main-wrapper">
-        <input type="checkbox" name="nav-menu-handle" id="nav-menu-handle" className="nav-menu-handle" />
-        <label htmlFor="nav-menu-handle"></label>
-
-        <nav role="navigation" className="nav-main">
-
-          <ul className="list-unstyled list-inline">
-            <li className="nav-item"><Link to={'/'}>Overview</Link></li>
-            <li className="nav-item"><Link to={'/processes'}>Processes</Link></li>
-            <li className="nav-item">
-              <a onClick={() => { handleEditUserProfile() }} className="has-icon has-icon-profile">
-                <span className="icon icon-profile"></span>
-                <span>Profile</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <Link to={'/logout'} className="has-icon has-icon-signout">
-                <span className="icon icon-signout"></span>
-                <span>Sign out</span>
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    )
-
-    if (!this.props.showProfile) {
-      profile = null
-    }
+    const {showProfile, handleEditUserProfile} = this.props
 
     return (
       <header className="header" role="banner">
@@ -45,7 +16,7 @@ export default React.createClass({
               <Link to={'/'} className='logo logo-concorda'></Link>
               <h2 className="m0">Concorda: Dashboard</h2>
             </div>
-            {profile}
+            {(showProfile ? <Menu handleEditUserProfile={handleEditUserProfile} /> : null)}
           </div>
         </div>
       </header>
