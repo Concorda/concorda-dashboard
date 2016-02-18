b62a382f44c90198ad3d13cc0764c037340bf503
malex <mircea.alexandru@gmail.com>
2016-02-18 14:28:41 +0200

    use external concorda plugin

--- server/backend.js
+++ server/backend.js
@@ -1,14 +1,25 @@
 'use strict'
 
+const Concorda = require('concorda')
+
 module.exports = function (server, options, next) {
   server.dependency('chairo')
+
+  var Config = require('../config/config.js')()
 
   // Set up our seneca plugins
   var seneca = server.seneca
 
+  // this should be changed after decree will support configuration
+  // and also decree will be integrated in concorda-dashboard
   seneca
     .use('mesh', {auto: true})
-    .use('concorda-api')
+    .use(Concorda, {
+      local: true,
+      'google-auth' : Config.googleLogin,
+      'twitter-auth': Config.twitterLogin,
+      'github-auth' : Config.githubLogin
+    })
 
   next()
 }
